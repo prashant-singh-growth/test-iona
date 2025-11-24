de3bf1954113719f4e281bd3ddb565a909a2441e
prashant-singh-web <prashant@growth.cx>
2025-11-24 15:36:44 +0530

    FAQ FIXES

--- src/pages/Alternative/AlternativeDynamic.jsx
+++ src/pages/Alternative/AlternativeDynamic.jsx
@@ -1,48 +1,48 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { AlternativePagesData } from '../../components/Data/AlternativeDynamic';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { AlternativePagesData } from "../../components/Data/AlternativeDynamic";
 
-import SectionDetails from '../../components/Pages/Alternative/SectionDetails';
-import SectionComparison from '../../components/Pages/Alternative/SectionComparison';
-import SectionWhySwitch from '../../components/Pages/Alternative/SectionWhySwitch';
-import SectionDifference from '../../components/Pages/Alternative/SectionDifference';
-import ResultSection from '../../components/Pages/LandingPage/ResultSection';
-import SectionChoose from '../../components/Pages/Alternative/SectionChoose';
-import CertificationSection from '../../components/Pages/SecLandingPage/CertificationSection';
-import TestimonialSlider from '../../components/utils/TestimonialSlider';
-import CTASection from '../../components/Pages/HomePage/CTASection';
-import FAQComponent from '../../components/utils/FAQComponent';
-import SeoHeader from '../../components/utils/SeoHeader';
-import AlternativeBanner from '../../components/Pages/Alternative/AlternativeBanner';
+import SectionDetails from "../../components/Pages/Alternative/SectionDetails";
+import SectionComparison from "../../components/Pages/Alternative/SectionComparison";
+import SectionWhySwitch from "../../components/Pages/Alternative/SectionWhySwitch";
+import SectionDifference from "../../components/Pages/Alternative/SectionDifference";
+import ResultSection from "../../components/Pages/LandingPage/ResultSection";
+import SectionChoose from "../../components/Pages/Alternative/SectionChoose";
+import CertificationSection from "../../components/Pages/SecLandingPage/CertificationSection";
+import TestimonialSlider from "../../components/utils/TestimonialSlider";
+import CTASection from "../../components/Pages/HomePage/CTASection";
+import FAQComponent from "../../components/utils/FAQComponent";
+import SeoHeader from "../../components/utils/SeoHeader";
+import AlternativeBanner from "../../components/Pages/Alternative/AlternativeBanner";
 
 function AlternativeDynamic() {
   const { Alternative } = useParams();
-  const AlternativeData = AlternativePagesData[Alternative]; 
+  const AlternativeData = AlternativePagesData[Alternative];
 
   if (!AlternativeData) return <h1>Case Study Not Found</h1>;
 
   return (
     <div>
-        <SeoHeader
+      <SeoHeader
         title={AlternativeData.seo.title}
         description={AlternativeData.seo.description}
-        />
-        <AlternativeBanner banner={AlternativeData.banner}/>
-        <SectionDetails detail={AlternativeData.details}/>
-        <SectionComparison comparison={AlternativeData.comparison}/>
-        <SectionWhySwitch switchData={AlternativeData.switch}/>
-        <SectionDifference/>
-        <ResultSection/>
-        <SectionChoose/>
-        <CertificationSection/>
-        <TestimonialSlider/>
-        <CTASection
+      />
+      <AlternativeBanner banner={AlternativeData.banner} />
+      <SectionDetails detail={AlternativeData.details} />
+      <SectionComparison comparison={AlternativeData.comparison} />
+      <SectionWhySwitch switchData={AlternativeData.switch} />
+      <SectionDifference />
+      <ResultSection />
+      <SectionChoose />
+      <CertificationSection />
+      <TestimonialSlider />
+      <CTASection
         title={"Switch to a Smarter PeopleStrong Alternative"}
-        description={"Stop juggling with disconnected HR tools. Try iona.ai and see why leading enterprises are choosing it as their preferred PeopleStrong Alternative. Experience end-to-end hiring automation with 100% accuracy and zero compliance risk."}
-
-        />
-        <FAQComponent FAQList={Alternative.FaqList}/>
-    
+        description={
+          "Stop juggling with disconnected HR tools. Try iona.ai and see why leading enterprises are choosing it as their preferred PeopleStrong Alternative. Experience end-to-end hiring automation with 100% accuracy and zero compliance risk."
+        }
+      />
+      <FAQComponent FAQList={AlternativeData.FaqList} />
     </div>
   );
 }
