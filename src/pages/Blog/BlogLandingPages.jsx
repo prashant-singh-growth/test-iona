e940306672c5437e825cad1a5db943c0c64d3be4
prashant-singh-web <prashant@growth.cx>
2026-04-08 16:58:25 +0530

    unused removes

--- src/pages/Blog/BlogLandingPages.jsx
+++ src/pages/Blog/BlogLandingPages.jsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import SeoHeader from "../../components/utils/SeoHeader";
 import { BlogList } from "../../components/Data/blog/BlogList";
 
