408f3cbc7ab28e32b0e0b4a839d3a8f1c549e5fe
prashant-singh-web <prashant@growth.cx>
2026-05-04 17:52:30 +0530

    new section added

--- src/components/Pages/HomePage/HowWeWork.jsx
+++ src/components/Pages/HomePage/HowWeWork.jsx
@@ -7,9 +7,9 @@ function HowWeWork() {
     <section className="w-full bg-transparent flex flex-col py-[60px] px-5 md:px-10 slg:px-[60px] lg:px-[90px] justify-center items-center space-y-10 fade-in-section">
       <div className="w-full max-w-[1440px] flex flex-col justify-center items-center space-y-10">
         {/* Heading */}
-        <div className="flex flex-col max-w-[750px] justify-center items-center space-y-4">
+        <div className="flex flex-col max-w-[650px] justify-center items-center space-y-4">
           <h2 className="text-3xl md:text-4xl lg:text-[40px] lg:leading-[50px] text-[#2A2564] font-bold md:font-semibold text-center">
-            Meet iona.ai: AI Enterprise Hiring Automation built for modern HR teams
+           See How We Transform Your HR Operations
           </h2>
         </div>
 
