cc552bece39279619e663c4f96a5d65ca4998ca8
prashant-singh-web <prashant@growth.cx>
2026-06-26 13:15:24 +0530

    new img added

--- src/components/Pages/HomePage/HowWeWork.jsx
+++ src/components/Pages/HomePage/HowWeWork.jsx
@@ -22,7 +22,7 @@ function HowWeWork() {
           loading="lazy"
         />
         <img
-          src="/assets/pages/homepage/home-design-2.webp"
+          src="/assets/pages/homepage/home-design-3.webp"
           alt="how iona work"
           title="how iona work"
           className="w-full md:hidden mx-auto max-w-[1440px] pt-10 pb-8 object-contain fade-in-delay"
