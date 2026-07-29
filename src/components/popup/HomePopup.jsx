6f438ff1e5d391de3285ff6d576a72af4ef1e3a6
prashant-singh-web <prashant@growth.cx>
2026-07-29 09:48:25 +0530

    fix

--- src/components/popup/HomePopup.jsx
+++ src/components/popup/HomePopup.jsx
@@ -17,12 +17,12 @@ function HomePopup() {
           animate={{ opacity: 1, y: 0, scale: 1 }}
           exit={{ opacity: 0, y: 30, scale: 0.95 }}
           transition={{ type: "spring", damping: 25, stiffness: 300 }}
-          className="fixed bottom-6 left-1/2  z-[9999] w-[calc(100%-2rem)] max-w-[1140px] !-translate-x-1/2"
+          className="fixed bottom-6 left-1/2  z-[9999] w-[calc(100%-2rem)] max-w-[1150px] !-translate-x-1/2"
         >
         
 
           {/* Glassmorphic Capsule Body */}
-          <div className="relative flex flex-col sm:flex-row items-center justify-between gap-4 lg:gap-2 rounded-lg  border !border-darkVoilet/20 !bg-white/50 p-3 pl-6 pr-4 shadow-xl !backdrop-blur-[4px]">
+          <div className="relative flex flex-col sm:flex-row items-center justify-between gap-4  rounded-lg  border !border-darkVoilet/20 !bg-white/50 p-3 pl-6 pr-4 shadow-xl !backdrop-blur-[4px]">
           
 
             {/* Content Text */}
